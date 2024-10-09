4630b2a72373c5f7e5ebe296618806ebe6ed369a
gabriel777k <bielv369@gmail.com>
2024-10-09 14:34:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Função para mostrar/esconder o conteúdo ao clicar no botão
 document.getElementById('readMoreBtn').addEventListener('click', function() {
   var moreInfo = document.getElementById('moreInfo');
-  if (moreInfo.classList.contains('hidden')) {
-    moreInfo.classList.remove('hidden');
+  if (moreInfo.style.display === "none" || moreInfo.style.display === "") {
+    moreInfo.style.display = "block";
     this.textContent = "Leia Menos";
   } else {
-    moreInfo.classList.add('hidden');
+    moreInfo.style.display = "none";
     this.textContent = "Leia Mais";
   }
 });
+
