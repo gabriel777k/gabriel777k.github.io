e6da35034591a0b03ff997d360f40e831fa72cf4
gabriel777k <bielv369@gmail.com>
2024-10-14 16:06:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ document.querySelectorAll('.leia-mais').forEach(button => {
     });
 });
 
+// Rolagem suave para âncoras
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
