4936081f04ff24a31ffcf20fbdf3d6bd4b24d17e
gabriel777k <bielv369@gmail.com>
2024-10-16 00:56:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,23 @@ signInWithEmailAndPassword(auth, email, password)
     const errorMessage = error.message;
   });
 
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDC3ZhmTqjd3P44uvuOeIQccNH8N5x20fE", // Verifique se essa chave está correta
+    authDomain: "verde-sabor.firebaseapp.com",
+    projectId: "verde-sabor",
+    storageBucket: "verde-sabor.appspot.com",
+    messagingSenderId: "803708320712",
+    appId: "1:803708320712:web:e225c98666c67a73bb9efd",
+    measurementId: "G-C3FCXHE4RX"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
