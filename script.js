874354c355d1235973ee36fa4a07fb5e9e7d1e31
gabriel777k <bielv369@gmail.com>
2024-10-16 02:04:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -59,33 +59,30 @@ console.log("Botão de login clicado");
 
 // Obtenha o modal
 var modal = document.getElementById("loginModal");
-if (modal) {
-  // Obtenha o botão que abre o modal
-  var btn = document.getElementById("loginButton");
-  
-  // Obtenha o elemento <span> que fecha o modal
-  var span = document.getElementsByClassName("close")[0];
-  
-  if (btn && span) {
-    // Quando o usuário clicar no botão de login, abre o modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
 
-    // Quando o usuário clicar no <span> (x), fecha o modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
+// Obtenha o botão que abre o modal
+var btn = document.getElementById("loginButton");
 
-    // Quando o usuário clicar em qualquer lugar fora do modal, fecha o modal
-    window.onclick = function(event) {
-      if (event.target == modal) {
+// Obtenha o elemento <span> que fecha o modal
+var span = document.getElementsByClassName("close")[0];
+
+// Quando o usuário clicar no botão de login, abre o modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Quando o usuário clicar no <span> (x), fecha o modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Quando o usuário clicar em qualquer lugar fora do modal, fecha o modal
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
-      }
+    }
 }
 
-  }
-}
 
 
   // Initialize Firebase
